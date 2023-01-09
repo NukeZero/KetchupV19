b259fd60b8d57ec74f4a66e8477311ca816a2ba5
Ketchup <29067524+NukeZero@users.noreply.github.com>
2023-01-09 14:20:03 -0500

    Initial commit.

---
+++ Source/_Common/kCommon.h
@@ -0,0 +1,57 @@
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+/*
+															`.-`
+													   .:/+oo++oo+/:-
+													.+sso++++///+++oss+-
+						 .                       `./yysooo++o--o+++oosyh+:-..``             ``.-://.
+						 shysoo+/:--..--:/++oshdmmmhyso/.-+syhhhs+-.:osyhdmmddhyysoo+////+osyyyhmmmy`
+						sdmmdyo+ooo+++++ooossssyyyhhy/. `:oshddhso:` `:syhossoooo+++++///////ohhydsyy`
+					  `sy+sh+syy+/:------::::://::hys/-` .:+osso+/- `-/syh:.::::::--....--/oys//ho//oy.
+					 `ys///ss///ss+:-.....---:::`+hhyyss+///+++++//+ssyyhhs `:::--.....-:os+::/so/::/oy:
+					.yo/:::/s+::::+s+:.....--:-  /hhhyyyssoooooooosssyyhhho  `-:--....-+o/---:/s/----:+s+.
+				   :s+:----:+s/--..-/o/-...--:`   ohhhyyyyssssysssyyyyhhhs`   `:--..-/o/.....-s/-..`...-/o+-
+				 :o+:-..``..-oo-.....-/+/:--::    `shhhhhhhhhyyhhhhyhhhhy`     ::::/+/-..```./o-.````...-://::.
+			   -++/-...````..:o:.```...-/+///:     `/yhhhhhhyyyyhhhhhhh+.      ://++:--.....-o:......--..``  `.
+			 :/--------......./+.....--::/+++:       `/yhhhhhhyhhhhhy/`        :++/:::------/+-----.`
+			``       ``..-----:+:-::-.....--:-          .:/yhhhhy/:.           `.````   .-::+/::.`
+						  `.-::++::.                       hNmmm.                         `:+-`
+							  .:+.                         mmmmo
+														  `mmmd`
+														  -mmm+
+														   yhs
+														  `hh/
+														  `hh-
+														  `yy`
+														   os.
+														  :s-
+														  .o-
+														   o:
+														   -+.`-`
+															`//so:`
+															  .++:-`
+															   .o+:-`
+																os+/
+																 -/-
+*/
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+#define	NEUZ_TITLE	"[VS22] Ketchup V19"				//: Window title of the client.
+#define	NEUZ_BHASH	"sunkist"							//: String to boot the client.
+#define	NEUZ_IPSET	"127.0.0.1"							//: Ip of the server, must match AccountServer.ini & LoginServer.ini.
+#define	NEUZ_CPORT	"5400"								//: Cache port, must match CacheServer.ini.
+#define	NEUZ_PHASH	"kikugalanet"						//: Md5 password salt.
+#define	NEUZ_MSGVR	"20100412"							//: Ver of the server, must match AccountServer.ini & LoginServer.ini.
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+#define	__FIX_STAT_BUFF									//: Fixes some issues present with the v19 spec_item.
+#define	__KIA_GOALDATA_FIX								//: Fixes quest arrow bug.
+#define __SLEEPING_FIX									//: Fixes the stun/sleeping bug present with Echoes of Weary.
+#define __SYS_SCR_CANCELLATION							//: Scroll of Unbinding.
+#define	__NAVIGATOR_V19									//: V19 Navigator.
+#define __NEW_EXCHANGE_V19								//: New exchange windows with arrow icons.
+#define	__NEW_TASKBAR_V19								//: V19 Taskbar.
+#define __TREASUREBOX									//: Retail like treasure box system.
+#define	__STATISTICS									//: Extra stats in the char window.
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
